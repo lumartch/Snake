385be361baf4316c34737ea96e1bde2227cc33ef
LumartCh <lumartch@gmail.com>
2019-11-20 21:57:12 -0600

    Se agregaron funciones a la clase SnakeBody para pintar el cuerpo del Snake en el mapa; Solo punta de momento la cabeza al centro de la pantalla.

--- snake.py
+++ snake.py
@@ -12,9 +12,13 @@ class Snake:
         # Variables para mostrar la pantalla
         self.window = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
+        # Instancia de la clase Snake
+        self.snake = SnakeBody(self.size, self.rows, self.window)
 
+    # Función para actualizar el estado de la pantalla
     def updateWindow(self):
         self.window.fill((0,0,0))   
+        self.snake.draw()
         self.drawGrid()
         pygame.display.update()
 
@@ -29,18 +33,41 @@ class Snake:
             pygame.draw.line(self.window, (255,255,255), (x,0), (x, self.width))
             pygame.draw.line(self.window, (255,255,255), (0,y), (self.width, y))
     
+    # Función para desplegar el gamplay completo del snake
     def gameplay(self):
         while self.flag:
             self.updateWindow()
             self.clock.tick(20)
             pygame.time.delay(50)
 
+# Clase para pintar el cuerpo de la serpiente en el mapa
 class SnakeBody:
-    def __init__(self):
-        pass
-    
+    def __init__(self, size, rows, window, colorHead = (255,255,255), colorBody = (47,79,79)):
+        # Variables para el manejo del cuerpo de la serpiente
+        self.body = []
+        self.size = size
+        self.rows = rows
+        self.head = (int(self.rows/2), int(self.rows/2))
+        self.body.append(self.head)
+        # Variables para el apartado gráfico de la serpiente
+        self.window = window
+        self.colorHead = colorHead
+        self.colorBody = colorBody
+
+    # Función para el movimiento de la serpiente dentro del mapa
     def move(self):
         pass
-
+    
+    # Dibuja el cuerpo de la serpiente en pantalla
     def draw(self):
-        pass
+        is_head = True
+        # Por cada elemento guardado en la variable "Body" se tomaran los ejex X y Y para pintarlos en pantalla
+        for b in self.body: 
+            i = b[0]
+            j = b[1]
+            dis = self.size / self.rows
+            if is_head:
+                pygame.draw.rect(self.window, self.colorHead, (i * dis + 1, j * dis + 1, dis, dis))
+                is_head = False
+            else:
+                pygame.draw.rect(self.window, self.colorBody, (i * dis + 1, j * dis + 1, dis, dis))
